0dd00d4082842ff81921e324cbdfa947ea1f4f87
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-02-24 22:01:28 -0600

    added routes

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(logger("dev"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-app.use(require("./public/api"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
