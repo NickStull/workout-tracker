f5db3b1ad1b1e922f21b09722804eeeb22dce06a
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-02-24 21:52:28 -0600

    added models

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
 
-const app = express;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
